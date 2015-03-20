7bb4eb29142e359ab8afe781253e5812fc4d1dde
Bin Zhang <binz@twitter.com>
2015-03-20 15:27:44 -0700

    bug fix of ResourcePool

--- pool.go
+++ pool.go
@@ -61,13 +61,13 @@ type ResourcePool struct {
 	closed chan struct{}
 }
 
-// New creates a new pool of Clients.
-func New(maxReserve, maxOpen uint32, opener ResourceOpener, m PoolMetrics) *ResourcePool {
+// NewPool creates a new pool of Clients.
+func NewPool(maxReserve, maxOpen uint32, opener ResourceOpener, m PoolMetrics) *ResourcePool {
 	if maxOpen < maxReserve {
 		panic("maxOpen must be > maxReserve")
 	}
 	tickets := make(chan struct{}, maxOpen)
-	for i := 0; i < maxOpen; i++ {
+	for i := uint32(0); i < maxOpen; i++ {
 		tickets <- struct{}{}
 	}
 	return &ResourcePool{
@@ -115,6 +115,7 @@ func (p *ResourcePool) GetWithTimeout(timeout time.Duration) (PooledResource, er
 	}
 	p.reportMetrics(time.Now().Sub(start))
 
+L:
 	for {
 		select {
 		case r := <-p.reserve:
@@ -125,7 +126,7 @@ func (p *ResourcePool) GetWithTimeout(timeout time.Duration) (PooledResource, er
 
 		default:
 			// no reserve
-			break
+			break L
 		}
 	}
 
@@ -183,6 +184,7 @@ func (p *ResourcePool) drainReserve() {
 		case r := <-p.reserve:
 			r.Close()
 		default:
+			return
 		}
 	}
 }
